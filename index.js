b9e65b9e6c617cfcfd7d945a99b4fc29d58f73e3
Erica Loret de Mola <elore023@yahoo.com>
2018-08-31 16:56:54 -0400

    added new page

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-const express = require('express')
-const app = express()
+let express = require('express');
+let app = express();
 
-app.get('/erica',(req, res) => res.send('Hello World'))
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
-app.listen(3000)
 
+app.get('/', function(req, res){
+
+    let dogs = [
+        {color:'black', breed: 'german shepherd', age:5},
+        {color:'white', breed: 'border collie', age:6},
+        {color:'mixed', breed: 'labrador retriever', age:8},
+        {color:'brown', breed: 'irish setter', age:3},
+        {color:'tri color', breed: 'bernese mountain dog', age:2}
+    ];
+
+    let addUpAllAges = function(array, prop) {
+        return array.reduce(function(a,b){
+            return a + b[prop];
+        }, 0)
+    }
+
+    let combinedAge = addUpAllAges(dogs, 'age')
+
+
+    let tagline = 'These are all my dogs';
+
+    res.render('pages/index', {
+        allMyDogs: dogs, 
+        tagline: tagline,
+        combinedAge: combinedAge
+    });
+
+});
+
+app.get('/about', function(req, res){
+    res.render('pages/about')
+})
+
+app.listen(3040);
